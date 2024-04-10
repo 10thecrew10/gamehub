76084d6d235e0316527acc759b4b383d5b24e1d2
Elisei <10thecrew01@gmail.com>
2024-04-10 12:41:46 +0200

    add center align for main window

--- run.py
+++ run.py
@@ -1,66 +1,109 @@
 import tkinter as tk
+from tkinter import Canvas, Scrollbar
 from PIL import Image, ImageTk
 from functools import partial
 
-# importing game runners
+# Importing game runners
 from games.snake.snake import run_snake
 from games.tetris.tetris import run_tetris
 from games.flappy_bird.main import run_flappy_bird
 
-# main window
+MAX_ITEMS_IN_ROW = 3
+IMAGE_SIZE = (150, 150)
+
+# Main window
 root = tk.Tk()
 
-# giving title to the main window
+# Title for the main window
 root.title("GameHub Menu")
 
-# Label is what output will be
-# show on the window
-label = tk.Label(root, text="Choose the game you want to play").pack()
+# Label for output on the window
+label = tk.Label(root, text="Choose the game you want to play")
+label.pack()
 
-games_frame = tk.Frame(root)
-games_frame.columnconfigure(0, weight=1)
-games_frame.columnconfigure(1, weight=1)
-games_frame.columnconfigure(2, weight=1)
+# Creating a canvas for buttons with scrollbar
+canvas = Canvas(root)
+canvas.pack(side="left", fill="both", expand=True)
 
+# Creating a frame for buttons inside the canvas
+games_frame = tk.Frame(canvas)
+canvas.create_window((0, 0), window=games_frame, anchor="nw")
 
-image = Image.open("games/snake/snake.png")  # Read the Image
-resize_image = image.resize((150, 150))  # Resize the image
-snake_picture = ImageTk.PhotoImage(resize_image)
+# Configuring columns in the frame
+for i in range(MAX_ITEMS_IN_ROW):
+    games_frame.columnconfigure(i, weight=1)
 
-image = Image.open("games/tetris/tetris.png")  # Read the Image
-resize_image = image.resize((150, 150))  # Resize the image
-tetris_picture = ImageTk.PhotoImage(resize_image)
+# Loading game images
+snake_image = ImageTk.PhotoImage(Image.open("games/snake/snake.png").resize(IMAGE_SIZE))
+tetris_image = ImageTk.PhotoImage(Image.open("games/tetris/tetris.png").resize(IMAGE_SIZE))
+flappy_bird_image = ImageTk.PhotoImage(Image.open("games/flappy_bird/flappy_bird.png").resize(IMAGE_SIZE))
 
-image = Image.open("games/flappy_bird/flappy_bird.png")  # Read the Image
-resize_image = image.resize((150, 150))  # Resize the image
-flappy_bird_picture = ImageTk.PhotoImage(resize_image)
-
-
+# Function to run games with exception handling
 def game_runner_decorator(game_runner: callable, current_window):
     def wrapper_function():
-        print('Called')
         try:
             current_window.withdraw()
             game_runner()
         except Exception as e:
             print('Error occured:', str(e))
-
-        current_window.deiconify()
+        finally:
+            current_window.deiconify()
 
     wrapper_function()
 
+# Creating buttons and labels containers
+buttons = []
+games_pack = [(snake_image, run_snake), (tetris_image, run_tetris), (flappy_bird_image, run_flappy_bird),
+              (snake_image, run_snake), (tetris_image, run_tetris), (flappy_bird_image, run_flappy_bird),
+              (snake_image, run_snake), (tetris_image, run_tetris), (flappy_bird_image, run_flappy_bird),
+              (snake_image, run_snake), (tetris_image, run_tetris), (flappy_bird_image, run_flappy_bird)]
 
-# add buttons
-btn1 = tk.Button(games_frame, image=snake_picture, command=partial(game_runner_decorator, run_snake, root))
-btn1.grid(row=0, column=0, sticky='we')
+games_pack = games_pack + games_pack.copy()
 
-btn2 = tk.Button(games_frame, image=tetris_picture, command=partial(game_runner_decorator, run_tetris, root))
-btn2.grid(row=0, column=1, sticky='we')
+button_container = None
+for i, (game_image, game_runner) in enumerate(games_pack):
+    game_name = ''
+    if game_runner == run_snake:
+        game_name = 'Snake'
+    elif game_runner == run_tetris:
+        game_name = 'Tetris'
+    elif game_runner == run_flappy_bird:
+        game_name = 'Flappy Bird'
 
-btn3 = tk.Button(games_frame, image=flappy_bird_picture, command=partial(game_runner_decorator, run_flappy_bird, root))
-btn3.grid(row=0, column=2, sticky='we')
+    button_container = tk.Frame(games_frame)
+    button_container.grid(row=i // MAX_ITEMS_IN_ROW, column=i % MAX_ITEMS_IN_ROW)
 
-games_frame.pack()
+    button = tk.Button(button_container, image=game_image, command=partial(game_runner_decorator, game_runner, root))
+    button.pack()
 
-root.eval('tk::PlaceWindow . center') # center main window
+    label = tk.Label(button_container, text=game_name)
+    label.pack()
+
+    buttons.append(button)
+
+# Adding scrollbar
+scrollbar = Scrollbar(root, orient="vertical", command=canvas.yview)
+scrollbar.pack(side="right", fill="y")
+
+canvas.configure(yscrollcommand=scrollbar.set)
+
+# Bind scrollbar to canvas
+canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+root.update() # place widgets
+
+container_width = button_container.winfo_reqwidth()
+container_height = button_container.winfo_reqheight()
+res_width = container_width * MAX_ITEMS_IN_ROW + MAX_ITEMS_IN_ROW * 10
+res_height = container_height * 4 + 4 * 6
+root.minsize(res_width, res_height) # set width and height for pretty view
+
+screen_width = root.winfo_screenwidth()  # Width of the screen
+screen_height = root.winfo_screenheight()  # Height of the screen
+
+# Calculate Starting X and Y coordinates for Window
+x = (screen_width / 2) - (res_width / 2)
+y = (screen_height / 2) - (res_height / 2)
+
+root.geometry('%dx%d+%d+%d' % (res_width, res_height, x, y))
 root.mainloop()
